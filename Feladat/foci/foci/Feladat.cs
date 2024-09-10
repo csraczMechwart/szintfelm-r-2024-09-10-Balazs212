457bd987a7ebfcebdf12f924a0d53e9464f76beb
Szabó Balázs <szabo.balazs@burg.lo.hu>
2024-09-10 13:22:01 +0200

    1. Feladat kész

---
+++ Feladat/foci/foci/Feladat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foci
+{
+    internal class Feladat
+    {
+        List<meccs> adatok = new List<meccs>();
+        public Feladat()
+        {
+            foreach (var item in File.ReadAllLines("meccs.txt", Encoding.UTF8).Skip(1))
+            {
+
+                string[] resz = item.Split(' ');
+                int fordulo = Convert.ToInt32(resz[0]);
+                int hazaiVeg = Convert.ToInt32(resz[1]);
+                int vendegVeg = Convert.ToInt32(resz[1]);
+                int hazaiFel = Convert.ToInt32(resz[1]);
+                int vendegFel = Convert.ToInt32(resz[1]);
+                string hazaiNev = resz[2];
+                string vendegNev = resz[3];
+                meccs Meccs = new meccs(fordulo, hazaiVeg, vendegVeg, hazaiFel, vendegFel, hazaiNev, vendegNev);
+                adatok.Add(Meccs);
+            }
+        }
+
+        public void Feladat2()
+        {
+
+        }
+    }
+}
---
+++ Feladat/foci/foci/meccs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foci
+{
+    internal class meccs
+    {
+        public meccs(int fordulo, int hazaiVeg, int vendegVeg, int hazaiFel, int vendegFel, string hazaiNev, string vendegNev)
+        {
+            this.fordulo = fordulo;
+            this.hazaiVeg = hazaiVeg;
+            this.vendegVeg = vendegVeg;
+            this.hazaiFel = hazaiFel;
+            this.vendegFel = vendegFel;
+            this.hazaiNev = hazaiNev;
+            this.vendegNev = vendegNev;
+        }
+
+        public int fordulo { get; set; }
+        public int hazaiVeg { get; set; }
+        public int vendegVeg { get; set; }
+        public int hazaiFel { get; set; }
+        public int vendegFel { get; set; }
+        public string hazaiNev {  get; set; }
+        public string vendegNev { get; set; }
+
+    }
+}
